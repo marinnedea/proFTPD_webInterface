657ac1a34895684ae9be2afe37bd53a07b59184d
Marin Nedea <mastigos@users.noreply.github.com>
2017-01-25 18:13:50 +0200

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-#Database Connection Here...
-
-$dbc = mysqli_connect('localhost', 'my_user', 'my_password', 'my_database') OR die('Could not connect because: '.mysqli_connect_error());
-
-?>
